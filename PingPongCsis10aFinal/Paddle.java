8cbe996955b4930290d646324a8df04b720b8de1
francisco1753 <frperez117503@gmail.com>
2024-05-02 17:11:07 -0700

    instead of making it in 1 class, i'll make classes for   each game function. already seems that the paddle class is finished

---
+++ PingPongCsis10aFinal/Paddle.java
@@ -0,0 +1,33 @@
+
+import javafx.scene.shape.Rectangle;
+import javafx.scene.paint.Color;
+
+/**
+ * Write a description of JavaFX class Paddle here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Paddle {
+    private Rectangle rectangle;
+    private static final int paddle_width = 15;
+    private static final int paddle_height = 80;
+    
+    public Paddle(double startX, double startY) {
+        rectangle = new Rectangle(paddle_width, paddle_height, Color.CRIMSON);
+        rectangle.setX(startX);
+        rectangle.setY(startY);
+    }
+    
+    public Rectangle getRectangle() {
+        return rectangle;
+    }
+    
+    
+    public void move(int dy, int windowHeight) {
+        double newY = rectangle.getY() + dy;
+        if (newY >= 0 && newY <= windowHeight - paddle_height) {
+            rectangle.setY(newY);
+        }
+    }
+}
